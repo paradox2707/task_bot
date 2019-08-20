6376a1e25468a364b34e4c2b6a21035047a66f5d
paradx2707 <sluva2707@gmail.com>
2019-08-20 20:29:27 +0300

    Add sent_message(not reply) and get message object

--- echo_bot.py
+++ echo_bot.py
@@ -4,12 +4,12 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Hello, human")
+	bot.send_message(message.chat.id, f"Hi, {message.chat.first_name}")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, "Катя, я тебе кохаю! ♥")
-
+	print(message)
 # @bot.message_handler(func=lambda message: True)
 # def echo_all(message):
 # 	bot.reply_to(message, message.text)
