0375beb641ebc561607e783df2d27920093bca8d
Андрій Слижук <sluva2707@gmail.com>
2020-04-17 01:00:50 +0300

    add test launch file without db (correcting)

--- echo_bot_1.py
+++ echo_bot_1.py
@@ -7,8 +7,8 @@ from xml.dom.xmlbuilder import _name_xform
 from telegramcalendar import create_calendar
 import telebot
 from telebot import types
-from ORMClass_STBot import *
 import re
+
 
 
 
@@ -17,32 +17,6 @@ bot = telebot.TeleBot(TOKEN)
 global_m_text = []
 
 
-def message_add(new_message):
-    # add message
-    create_message = Message()
-    create_message.message_id = new_message.message_id
-    create_message.date = datetime.datetime.fromtimestamp(new_message.date)
-    create_message.text = new_message.text
-    create_message.add()
-
-    # add FromSomeone
-    create_someone = FromSomeone()
-    create_someone.message_id = new_message.message_id
-    create_someone.someone_id = new_message.from_user.id
-    create_someone.is_bot = new_message.from_user.is_bot
-    create_someone.first_name = new_message.from_user.first_name
-    create_someone.last_name = new_message.from_user.last_name
-    create_someone.username = new_message.from_user.username
-    create_someone.language_code = new_message.from_user.language_code
-    create_someone.add()
-
-    # add Entities
-    # create_entities = Entities()
-    # create_entities.message_id = new_message.message_id
-    # create_entities.length = new_message.entities.length
-    # create_entities.offset = new_message.entities.offset
-    # create_entities.type = new_message.entities.type
-    # create_entities.add()
 
 @bot.inline_handler(lambda query: len(query.query) is 0)
 def empty_query(query):
@@ -53,7 +27,7 @@ def empty_query(query):
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.send_message(message.chat.id, f"Hi, {message.chat.first_name}")
-    message_add(message)
+
 
     # print(message)
     # n_book = Book()
